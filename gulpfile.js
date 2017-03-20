50ae70866bbe0d8a63d8b9248df846c6e6203278
Daniel Peppicelli <Daniel Peppicelli>
2017-03-20 16:32:43 -0500

    Gulp, Jquery & Bootstrap addition
    Now the theme uses bootstrap, Gulp and Jquery. For the moment,
    these packages are installed via NPM (npm install) and then copied
    to the static theme folder by a pretty dummy Gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require('gulp');
+var _    = require('lodash');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('copy-assets', function() {
+    var assets = {
+        js: [
+            './node_modules/bootstrap/dist/js/bootstrap.min.js',
+            './node_modules/jquery/dist/jquery.min.js'
+        ],
+        css: ['./node_modules/bootstrap/dist/css/bootstrap.min.css']
+    };
+    _(assets).forEach(function (assets, type) {
+       gulp.src(assets).pipe(gulp.dest('./static/'+type));
+    });
 });
