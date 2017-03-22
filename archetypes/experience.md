61bfe24933ffcdc1114427ac0452c8e9f750273e
Daniel Peppicelli <Daniel Peppicelli>
2017-03-22 16:52:01 -0500

    Support (proto) for experiences
    - Added font awesome npm package
    - Added a new Archetype for "experiences"
    - The new archetype get rendered on the front page

---
+++ archetypes/experience.md
@@ -0,0 +1,9 @@
++++
+company = ""
+logo = ""
+title = ""
+start_date = ""
+end_date = ""
+responsabilities = []
+main_achievements = []
++++
--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,11 @@ gulp.task('copy-assets', function() {
             './node_modules/bootstrap/dist/js/bootstrap.min.js',
             './node_modules/jquery/dist/jquery.min.js'
         ],
-        css: ['./node_modules/bootstrap/dist/css/bootstrap.min.css']
+        css: [
+            './node_modules/bootstrap/dist/css/bootstrap.min.css',
+            './node_modules/font-awesome/css/font-awesome.min.css'
+        ],
+        fonts: ['./node_modules/font-awesome/fonts/*']
     };
     _(assets).forEach(function (assets, type) {
        gulp.src(assets).pipe(gulp.dest('./static/'+type));
